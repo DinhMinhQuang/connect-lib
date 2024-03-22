8648b857f050921c85e883e5b69c5560abdecca2
MinhQuang <quangdm@payme.vn>
2024-03-22 16:57:39 +0700

    remove config

--- src/env/Config.java
+++ src/env/Config.java
@@ -1,22 +1,11 @@
 package env;
 
 public class Config {
-    public static String publicKeyPayME = "-----BEGIN PUBLIC KEY-----\n"
-            + "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAK//Ost3vYkmPXuAlIf0jVnfJjVraljw\n"
-            + "gY52hx6I1R3de1zLxIMOZPIYaDa+yxuMyyxNy/iDqcnE5GbKh/GHdn8CAwEAAQ==\n"
-            + "-----END PUBLIC KEY-----";
+    public static String publicKeyPayME = "";
 
-    public static String privateKey = "-----BEGIN RSA PRIVATE KEY-----\n"
-            + "MIIBOgIBAAJBALnXgEAHajptceS3CA1PZVCypx4tkNTUdRsY+vgCwkzx21Qj/7g9\n"
-            + "FV7sVRvivUZ4mt/zbAHQtc5gm1bMIQZJa9UCAwEAAQJADH/qKGRXSMbDulZ1PC/y\n"
-            + "6JKbmvQFocsIdWIgvz2wQ+yDKfgwIWH9qP17dMvwUfd01dNai/LtK4IQNdLs+C3E\n"
-            + "AQIhAOYKusFQRcsCYauMcBXMfU6Ov7Byeam5g93AWyXRMUH1AiEAzs/0CkdR8emZ\n"
-            + "vHocZ3tjcLbwzqZArlI8O/fTx+uG9mECIQCRI7Hi6Aew50bCWrAZQNTKrMwKwp86\n"
-            + "U578WTHo8Uy3xQIgMWu2SKKEbYfCKi0QDpaIy82bu/Y0rLOQG1B8tmCNLuECIFBK\n"
-            + "Edpz9mhjTDmL6yEyewBFWaD+GyY2C4WvvTI+N8tk\n"
-            + "-----END RSA PRIVATE KEY-----";
+    public static String privateKey = "";
 
-    public static String xApiClient = "143090043042";
+    public static String xApiClient = "";
 
     public static String Authorization = "";
 }
